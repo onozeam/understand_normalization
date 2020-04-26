49b7bcd03952419520e1740f5d63a0a54862f6da
maezono <maezonoinfo@gmail.com>
2020-04-26 18:37:32 +0900

    fix and refactor

---
+++ README.md
@@ -0,0 +1,1 @@
+ref: [Understanding the backward pass through Batch Normalization Layer](https://kratzert.github.io/2016/02/12/understanding-the-gradient-flow-through-the-batch-normalization-layer.html)
--- main.py
+++ main.py
@@ -4,29 +4,46 @@ import numpy as np
 
 class BatchNorm:
     def __init__(self, seq_size):
-        self.w = np.random.randn(seq_size, seq_size)
+        # params
+        self.w = np.random.randn(seq_size)
         self.b = np.zeros(seq_size)
         self.epsilon = 0.00001
-        # todo: backward
-        pass
+        # cache
+        self.x, self.mu, self.var = None, None, None
 
     def __call__(self, args):
         return self.forward(args)
 
-    def forward(self, inp):
-        mu = np.average(inp, axis=0)
-        var = np.var(inp, axis=0)
-        normed_inp = (inp-mu)/np.sqrt(var+self.epsilon)
-        out = np.dot(normed_inp, self.w) + self.b
+    def forward(self, x):
+        """
+        Shape
+            x: (batch_size, seq_size)
+            out: (batch_size, seq_size)
+        """
+        mu = np.average(x, axis=0)
+        var = np.var(x, axis=0)
+        normlized_x = (x-mu) / np.sqrt(var+self.epsilon)
+        out = normlized_x*self.w + self.b
         return out
+
+    def backward(self, dout):
+        """
+        Shape
+            dout: (batch_size, seq_size)
+            dx: (batch_size, seq_size)
+            dw: (seq_size)
+            db: (seq_size)
+        """
+        pass
 
 
 def main():
     N, S = 32, 124
-    inp = np.random.randn(N, S)
+    x = np.random.randn(N, S)
     batch_norm_layer = BatchNorm(S)
-    out = batch_norm_layer(inp)
+    out = batch_norm_layer(x)
     print(out.shape)
+    print(out)
 
 
 if __name__ == '__main__':
