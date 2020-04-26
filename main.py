7e70d79174b74489676980c990307a7cc461e6e7
maezono <maezonoinfo@gmail.com>
2020-04-26 19:09:43 +0900

    backward and refactor

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class BatchNorm:
         var = np.var(x, axis=0)
         normlized_x = (x-mu) / np.sqrt(var+self.epsilon)
         out = normlized_x*self.w + self.b
+        self.x, self.mu, self.var = x, mu, var  # caching
         return out
 
     def backward(self, dout):
@@ -34,16 +35,25 @@ class BatchNorm:
             dw: (seq_size)
             db: (seq_size)
         """
-        pass
+        dh = np.sqrt(self.var+self.epsilon) * (dout*self.w - (self.x-self.mu) * (self.var+self.epsilon) * np.average((self.x-self.mu) * dout*self.w, axis=0))
+        dx = dh - np.average(dh, axis=0)
+        dw = np.average((self.x-self.mu) / np.sqrt(self.var+self.epsilon) * dout, axis=0)
+        db = np.average(dout, axis=0)
+        return dx, dw, db
 
 
 def main():
     N, S = 32, 124
-    x = np.random.randn(N, S)
+    x, dout = np.random.randn(N, S), np.random.randn(N, S)
     batch_norm_layer = BatchNorm(S)
     out = batch_norm_layer(x)
-    print(out.shape)
-    print(out)
+    print('# forward')
+    print(f' - out.shape: {out.shape}')
+    dx, dw, db = batch_norm_layer.backward(dout)
+    print('# backward')
+    print(f' - dx.shape: {dx.shape}')
+    print(f' - dw.shape: {dw.shape}')
+    print(f' - db.shape: {db.shape}')
 
 
 if __name__ == '__main__':
